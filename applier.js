f6404221ac2699b7c592e4f0925feb203e6482f7
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2021-10-22 19:56:21 -0300

    change color for a specific point

--- applier.js
+++ applier.js
@@ -3,27 +3,29 @@ Perceptron = require('./Perceptron');
 Data2d = require('./dataGenerator');
 
 //Generates a data set
-var data = new Data2d(100);
+var data = new Data2d(10);
 
 //Generates a categorized data points
 var pts2d = data.points();
+
 //Generates graphical cartesian points
 var arrCats = ['Cat1','Cat2'];
 var plotPts = data.plot(pts2d, c1=arrCats[0], c2=arrCats[1]);
-plt.stack(plotPts)
+
 //Train the Percetron
 p = new Perceptron(2, 0.001); //n = 2 -> it's a 2d dataset
 let initWeights = p.getWeights();
 console.log(`Initial Weights: ${initWeights}`);
 
-for (let i =0; i < pts2d.lenght; i++){
-  let inputs = [pts2d[i].x, pts2d[i].y];
-  let target = pts2d[i].label
+for (point of pts2d){
+  let inputs = [point.x, point.y];
+  let target = point.label
   p.train(inputs, target)
   //Visualize training on the fly
   let guess = p.guess(inputs)
   if (guess != target){
-
+    plotPoint = data.changeColor(point, arrCats);
+    plt.plot(plotPoint);
   }
 }
 
