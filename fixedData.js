c0f7b72d19bca36d61a2b04c3d79e043c41c3026
Cid Medeiros <46410491+cidmedeiros@users.noreply.github.com>
2021-10-24 18:58:36 -0300

    create statical data to comparison

---
+++ fixedData.js
@@ -0,0 +1,29 @@
+
+Data2d = require('./dataGenerator');
+const fs = require('fs');
+
+function generateFixedData(n){
+    //n -> int indicates the number of data points to create
+    //Generates a data set
+    var data = new Data2d(n);
+
+    //Generates a categorized data points
+    var pts2d = data.points();
+
+    //Generates graphical cartesian points
+    var arrCats = ['Cat1','Cat2'];
+    var plotPts = data.plot(pts2d, c1=arrCats[0], c2=arrCats[1]);
+    FixData = {cats: arrCats, points:plotPts};
+
+    //saves to local file
+    const jsonString = JSON.stringify(FixData);
+    fs.writeFile('./data.json', jsonString, err => {
+        if (err) {
+            console.log('Error writing file', err);
+        } else {
+            console.log('Successfully wrote file');
+        }
+    });
+}
+
+generateFixedData(100);
